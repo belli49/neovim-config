16cc66a44e1bdb3b76edcc84ffb73f764284ae88
belli49 <komessu@DESKTOP-J01DJLA>
2023-12-07 16:25:55 +0900

    Added bash parser for TS

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"vimdoc", "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "make", "cpp"},
+  ensure_installed = {"vimdoc", "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "make", "cpp", "typescript", "bash"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
