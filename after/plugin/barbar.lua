471cd8e7e33b62ab4a2a75467409ddc1f89e484a
belli49 <pedrokomessu@gmail.com>
2023-12-07 22:48:02 +0900

    General fixups

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -3,7 +3,9 @@ local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
 map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader><BS>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<leader><Tab>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
 map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
 map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
@@ -22,6 +24,7 @@ map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
 map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
 map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<leader>bd', '<Cmd>BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
