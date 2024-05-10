5c1a01b44ef59272a86e3fda5c879c4ec1568ee2
belli49 <59339268+belli49@users.noreply.github.com>
2024-05-10 15:27:02 +0900

    Delete nvim directory

--- nvim/after/plugin/navbuddy.lua
+++
@@ -1,9 +0,0 @@
-local navbuddy = require("nvim-navbuddy")
-
-require("lspconfig").clangd.setup {
-    on_attach = function(client, bufnr)
-        navbuddy.attach(client, bufnr)
-    end
-}
-
-vim.keymap.set('n', '<leader>pd', '<cmd>Navbuddy<CR>')
